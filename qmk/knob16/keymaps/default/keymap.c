68cf8cbf234261b25188b0369217a3eb772bbdbf
NewYorkDalz <150974282+NewYorkDalz@users.noreply.github.com>
2023-11-16 10:52:29 -0500

    Update keymap.c
    C1 keyboard shortcuts

--- qmk/knob16/keymaps/default/keymap.c
+++ qmk/knob16/keymaps/default/keymap.c
@@ -2,21 +2,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+    ⇧-	⌘K	⇧=	⇧⌘-	W	⇧⌘=	⌃⌥⇧⌘X	⌃⌥⇧⌘S	⌃⌥⇧⌘C	⌃⌥⇧⌘W	⌃⌥⇧⌘D	⌃⌥⇧⌘E																																								
+    ⌃⌥⇧⌘[	⌘E	⌃⌥⇧⌘]	⌃⇧⌘-	⌃⌘F	⌃⇧⌘+	⌃⌥⇧⌘-	⌥⌘0	⌃⌥⇧⌘=	⌃⌥⌘-	⇧⌘D	⌃⌥⌘+																																								
+    ⌃⌥⌘V	⇧⌘C	⌃⌥⌘B	⌃⌥⇧⌘K	⇧⌘V	⌃⌥⇧⌘L	⌃⌥⌘Q	⌘G	⌃⌥⌘W	⌃⌥⇧⌘O	⌘⌫	⌃⌥⇧⌘P																																								
+    [	B	]	⌥[	E	⌥]	⌥⇧⌘L	⌥R	⌥⇧⌘R	⌘←	Equals	⌘→																																								
      */
     [0] = LAYOUT_ortho_4x4(
-        KC_TRNS, KC_P7  , KC_TRNS,     KC_TRNS, KC_P8  , KC_TRNS,     KC_TRNS, KC_P9  , KC_TRNS,     KC_TRNS, KC_PSLS, KC_TRNS,
-        KC_TRNS, KC_P4  , KC_TRNS,     KC_TRNS, KC_P5  , KC_TRNS,     KC_TRNS, KC_P6  , KC_TRNS,     KC_TRNS, KC_PAST, KC_TRNS,
-        KC_TRNS, KC_P1  , KC_TRNS,     KC_TRNS, KC_P2  , KC_TRNS,     KC_TRNS, KC_P3  , KC_TRNS,     KC_TRNS, KC_PMNS, KC_TRNS,
-        KC_TRNS, KC_P0  , KC_TRNS,     KC_TRNS, KC_PDOT, KC_TRNS,     KC_TRNS, KC_PENT, KC_TRNS,     KC_TRNS, KC_PPLS, KC_TRNS
+        S(KC_MINS), G(KC_K), S(KC_EQL),     SGUI(KC_MINS), KC_W, SGUI(KC_PEQL),     HYPR(KC_X), HYPR(KC_S), HYPR(KC_C),     HYPR(KC_W), HYPR(KC_D), HYPR(KC_E),
+        HYPR(KC_LBRC), G(KC_E), HYPR(KC_RBRC),    C(S(G(KC_MINS))), C(G(KC_F)),	C(S(G(KC_PPLS))),    HYPR(KC_MINS), RAG(KC_0),	HYPR(KC_EQL),    LCAG(KC_MINS),	G(KC_D), LCAG(KC_PPLS)        
+        LCAG(KC_V), LSG(KC_C), LCAG(KC_B),    HYPR(KC_K), LSG(KC_V), HYPR(KC_L),	LCAG(KC_Q),	G(KC_G), LCAG(KC_W),	HYPR(KC_O), G(KC_BSPC), HYPR(KC_P), 																																								
+        KC_LBRC, KC_B, KC_RBRC,    A(KC_LBRC), KC_E, A(KC_RBRC),	S(A(G(KC_L))), A(KC_R), S(A(G(KC_R))),    G(KC_LEFT), KC_EQL, G(KC_RIGHT)																																								
     ),
 
     [1] = LAYOUT_ortho_4x4(
@@ -71,3 +66,10 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return false;
 } 
+    /* Space savers */
+uint16_t keycode_config(uint16_t keycode) {
+    return keycode;
+}
+uint8_t mod_config(uint8_t mod) {
+    return mod;
+}
